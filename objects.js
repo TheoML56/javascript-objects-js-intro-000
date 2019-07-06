d6756b8364d387759426d233f67fe77bb9435e6f
Theo Malaper <theomalaper.cognez@gmail.com>
2019-07-06 00:55:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
 var playlist = { artistName: 'songTitle' }
+
+function udpatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
+
